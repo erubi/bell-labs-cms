9cf0bfe2a6c67ac8a7d8db20bb0b0907ea8b4199
Evan Rubinton <evanrubinton@gmail.com>
2015-02-13 15:40:14 -0500

    Begin writing event spec before figuring out reccurrence issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,17 @@
 class Event < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :datetime, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :valid_end_date
 
 
   private
 
   def event_attrs
-
   end
+
+  def valid_end_date
+  end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-  it "has a start time and end time" do
+  it { should validate_presence_of :start_time }
+  it { should validate_presence_of :end_time }
 
-  end
 end
