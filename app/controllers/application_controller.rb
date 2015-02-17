efc101204558efc82c50288b575c3f2989933e0c
Evan Rubinton <evanrubinton@gmail.com>
2015-02-17 11:19:04 -0500

    Add nav bar partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def require_signed_in
+    if !signed_in?
+      redirect_to landing_url
+    end
+  end
+
+  def require_signed_out
+    if signed_in?
+      redirect_to root_url
+    end
+  end
 end
