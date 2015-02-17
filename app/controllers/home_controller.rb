e4599329d6e9ed7e7c9055f7fbf764a904030f77
Evan Rubinton <evanrubinton@gmail.com>
2015-02-17 10:39:43 -0500

    Create home controller and views, add bootstrap sass gem

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  before_action :require_signed_in, only: [:root]
+  before_action :require_signed_out, only: [:landing]
+
+  def root
+  end
+
+  def landing
+  end
+end
