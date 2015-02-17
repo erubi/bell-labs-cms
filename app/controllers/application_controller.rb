70c0ff584a5bce4b8fdfb2e6860d85ca4b0faed1
Evan Rubinton <evanrubinton@gmail.com>
2015-02-17 11:42:14 -0500

    Fix sign out link error, add correct redirect paths for sign in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,12 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
     end
   end
+
+  def after_sign_in_path_for(resource)
+    root_url
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    landing_url
+  end
 end
