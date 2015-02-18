11bf1ede1ee195c6f1bbcba28185eedf007f4cb6
Evan Rubinton <evanrubinton@gmail.com>
2015-02-18 13:25:05 -0500

    Use master branch of carrierwave gem, add rspec for media module images

--- spec/models/media_module_spec.rb
+++ spec/models/media_module_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe MediaModule, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:media_module)).to be_valid
   end
+
+  it "can have images" do
+    media_module = FactoryGirl.create(:media_module)
+    media_module.images = [Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'images', 'Bell_Labs_Holmdel.jpg' ))]
+
+    expect(media_module.images.length).to equal(1)
+  end
 end
