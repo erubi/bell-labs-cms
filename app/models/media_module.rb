21ac079df54b82223ca06f381732b72c8c437e1f
Evan Rubinton <evanrubinton@gmail.com>
2015-02-18 14:25:09 -0500

    Add video rspec for media module

--- app/models/media_module.rb
+++ app/models/media_module.rb
@@ -2,4 +2,5 @@ class MediaModule < ActiveRecord::Base
   validates :name, presence: true
 
   mount_uploaders :images, ImageUploader
+  mount_uploaders :videos, VideoUploader
 end
--- spec/models/media_module_spec.rb
+++ spec/models/media_module_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MediaModule, type: :model do
+  subject(:media_module) { FactoryGirl.create(:media_module)}
+
   it "has a valid factory" do
-    expect(FactoryGirl.create(:media_module)).to be_valid
+    expect(media_module).to be_valid
   end
 
   it "can have images" do
-    media_module = FactoryGirl.create(:media_module)
-    media_module.images = [Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'images', 'Bell_Labs_Holmdel.jpg' ))]
+    media_module.images = [Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'images', 'sample1.jpg' ))]
 
     expect(media_module.images.length).to equal(1)
   end
+
+  it "can have videos" do
+    media_module.videos = [Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'fixtures', 'videos', 'sample1.mp4' ))]
+
+    expect(media_module.videos.length).to equal(1)
+  end
+
 end
