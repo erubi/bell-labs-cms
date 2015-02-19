e1a4a33159a17b86903eafa4407384c0fae4f0e6
Evan Rubinton <evanrubinton@gmail.com>
2015-02-19 11:21:05 -0500

    Whitelist video formats, add seed data

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,2 +1,3 @@
 class Api::EventsController < ApplicationController
+  before_action :require_signed_in
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+mm1 = MediaModule.create(name: "Lulz module", active_interval: 20, active_time: 4)
+mm1.images = [File.open("#{Rails.root}/spec/fixtures/images/sample1.jpg")]
+
+mm2 = MediaModule.create(name: "Doge party", active_interval: 20, active_time: 4)
+mm2.videos = [File.open("#{Rails.root}/spec/fixtures/videos/sample1.mp4")]
+
+users = User.create([{email: 'dude@example.com', password: 'password'}])
