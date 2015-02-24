6b8b9cde7a4377110a9eafa86f04c37e16a3cdbf
Evan Rubinton <evanrubinton@gmail.com>
2015-02-24 12:07:19 -0500

    Add back devise token auth concern to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+
+  before_validation -> do
+    self.uid = SecureRandom.uuid
+  end
+
   # Include devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
 
-  # including this concern causes uid to be required and raises error immediately
-  # this concern does have potentially useful methods though
-  # include DeviseTokenAuth::Concerns::User
-
-  validates :email, presence: true
-
-  # before_save -> do
-  #   self.uid = SecureRandom.uuid
-  #   skip_confirmation!
-  # end
+  include DeviseTokenAuth::Concerns::User
 end
