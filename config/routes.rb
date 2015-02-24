29acaa5956a04445fa48d98fbbd4f2b242329914
Evan Rubinton <evanrubinton@gmail.com>
2015-02-24 11:23:15 -0500

    Install devise token auth gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'landing',  to: "home#landing", as: :landing
 
   devise_for :users, controllers: { sessions: "users/sessions" }
+  mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :api, defaults: { format: :json } do
     resources :events, except: [:new, :edit]
