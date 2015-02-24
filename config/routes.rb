b1bb56bd5f35695f4578e7718e818a067c005f8f
Evan Rubinton <evanrubinton@gmail.com>
2015-02-24 11:29:21 -0500

    Add columns to users for devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'landing',  to: "home#landing", as: :landing
 
   devise_for :users, controllers: { sessions: "users/sessions" }
-  mount_devise_token_auth_for 'User', at: 'auth'
 
   namespace :api, defaults: { format: :json } do
     resources :events, except: [:new, :edit]
     resources :media_modules, except: [:new, :edit]
+    mount_devise_token_auth_for 'User', at: 'auth'
   end
 end
--- db/migrate/20150224155403_devise_token_auth_create_users.rb
+++ db/migrate/20150224155403_devise_token_auth_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
   def change
-    add_column :users, :provider, :string, null: false
-    add_column :users, :uid, :string, null: false, default: ""
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string, default: ""
     add_column :users, :tokens, :text
 
     add_index :users, [:uid, :provider],     :unique => true
