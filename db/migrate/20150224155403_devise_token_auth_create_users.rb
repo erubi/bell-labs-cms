2a5e1539ddf24e1c8af4769c9fa20f78a56446a6
Evan Rubinton <evanrubinton@gmail.com>
2015-02-25 13:00:14 -0500

    Remove devise token auth migration file

--- db/migrate/20150224155403_devise_token_auth_create_users.rb
+++
@@ -1,9 +0,0 @@
-class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string, default: ""
-    add_column :users, :tokens, :text
-
-    add_index :users, [:uid, :provider],     :unique => true
-  end
-end
