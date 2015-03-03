97a91a97013322e84ffb548440b3490df852975c
Evan Rubinton <evanrubinton@gmail.com>
2015-03-03 17:09:09 -0500

    Add countdown begin datetime attr to event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218182848) do
+ActiveRecord::Schema.define(version: 20150303212546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 20150218182848) do
     t.string   "name"
     t.text     "description"
     t.datetime "start_time"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.datetime "end_time"
+    t.datetime "countdown_begin"
   end
 
   create_table "media_modules", force: :cascade do |t|
