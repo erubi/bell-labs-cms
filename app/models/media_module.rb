10ac90fb94db98c7b556bcc85df4a197e4d59e73
Evan Rubinton <evanrubinton@gmail.com>
2015-03-12 21:39:18 -0400

    Add server side validation for total of weight less than 100%

--- app/models/media_module.rb
+++ app/models/media_module.rb
@@ -1,12 +1,18 @@
 class MediaModule < ActiveRecord::Base
   validates :name, :weight, presence: true
   validates :weight, inclusion: { in: 0..1 }
+  validate :weight_total
+
   before_update :activate_callback
 
   mount_uploaders :images, ImageUploader
   mount_uploaders :videos, VideoUploader
 
   scope :active, -> {where(active: true)}
+
+  def other_modules
+    MediaModule.where.not(id: self.id)
+  end
 
   def activate_callback
     if self.active
@@ -15,7 +21,14 @@ class MediaModule < ActiveRecord::Base
   end
 
   def deactivate_other_modules
-    others = MediaModule.where.not(id: self.id)
-    others.update_all(active: false)
+    other_modules.update_all(active: false)
+  end
+
+  private
+
+  def weight_total
+    if (other_modules.sum(:weight) + self.weight) > 1
+      errors.add(:base, "Total sum of weight must not exceed 100%")
+    end
   end
 end
