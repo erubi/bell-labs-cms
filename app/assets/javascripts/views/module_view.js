373255e743ed5285b62aa0d93b2d229ffad2e462
Evan Rubinton <evanrubinton@gmail.com>
2015-03-12 20:06:59 -0400

    Handle error display in module/scene views'

--- app/assets/javascripts/views/module_view.js
+++ app/assets/javascripts/views/module_view.js
@@ -4,11 +4,12 @@ BellCMS.Views.ModuleItemView = Marionette.ItemView.extend({
   template: 'exterior_modules/module_item',
 
   initialize: function(){
-    // this.listenTo(this.model, "sync", this.render)
+    this.listenTo(this.model, "invalid", this.showError);
   },
 
   events: {
-    'change .weight-input' : 'updateModelWeight'
+    'change .weight-input' : 'updateModelWeight',
+
   },
 
   updateModelWeight: function(event){
@@ -16,8 +17,19 @@ BellCMS.Views.ModuleItemView = Marionette.ItemView.extend({
     this.model.set('weight', weight);
 
     if (this.model.isValid()){
+      this.removeError();
       this.model.save();
     }
+  },
+
+  showError: function(event){
+    var error = this.model.validationError;
+    this.removeError();
+    this.$el.prepend('<div class="alert alert-warning">'+error+'</div>');
+  },
+
+  removeError: function(){
+    this.$el.find('.alert').remove();
   }
 
 });
