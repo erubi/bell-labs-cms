25f192ba3a2f0e662c69501e79e733819fba9be1
Evan Rubinton <evanrubinton@gmail.com>
2015-03-19 13:22:10 -0400

    implement delete for event views

--- app/assets/javascripts/views/event_item_view.js
+++ app/assets/javascripts/views/event_item_view.js
@@ -8,5 +8,22 @@ BellCMS.Views.EventItemView = Marionette.ItemView.extend({
       startISO: this.model.startISO(),
       endISO: this.model.endISO()
     }
+  },
+
+  events: {
+    'click .delete-event-btn' : 'deleteEvent'
+  },
+
+  modelEvents: {
+    'destroy' : 'modelDestroyed'
+  },
+
+  modelDestroyed: function(){
+    this.destroy();
+  },
+
+  deleteEvent: function(event){
+    event.preventDefault();
+    this.model.destroy();
   }
 });
