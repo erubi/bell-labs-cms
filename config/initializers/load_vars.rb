3fe915a9d14cd70371d5365b564e0c0e11f84257
Evan Rubinton <evanrubinton@gmail.com>
2015-03-22 13:57:33 -0400

    move app yml load back to app rb

--- config/initializers/load_vars.rb
+++ config/initializers/load_vars.rb
@@ -2,5 +2,3 @@ SETTINGS = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 
 SETTINGS['VIDEO_PLAYER_DURATION'] = MediaModule.find_by(name: "Video Player").movie_duration
 
-ENV.update YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
-
