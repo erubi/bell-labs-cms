58559531033b29faa33f225b13de7b98c31867d9
Evan Rubinton <evanrubinton@gmail.com>
2015-03-22 10:39:35 -0400

    update media content view markup and style

--- app/assets/javascripts/views/media_nav_view.js
+++ app/assets/javascripts/views/media_nav_view.js
@@ -1,6 +1,6 @@
 BellCMS.Views.MediaNavView = Marionette.ItemView.extend({
   tagName: 'nav',
-  className: 'navbar navbar-default navbar-app',
+  className: 'top-media-nav navbar navbar-default navbar-app',
   template: 'media/nav_bar',
 
   events: {
