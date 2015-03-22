903ec8d0ee1cc60f064eee32e046ac201362ac98
Evan Rubinton <evanrubinton@gmail.com>
2015-03-22 15:15:11 -0400

    env check

--- config/initializers/load_vars.rb
+++ config/initializers/load_vars.rb
@@ -1,4 +1,6 @@
 SETTINGS = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 
-SETTINGS['VIDEO_PLAYER_DURATION'] = MediaModule.find_by(name: "Video Player").movie_duration
+if Rails.env.development? || Rails.env.production?
+  SETTINGS['VIDEO_PLAYER_DURATION'] = MediaModule.find_by(name: "Video Player").movie_duration
+end
 
