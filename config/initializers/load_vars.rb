3632c49bc86b6f9e35717e54852b99a23da1eedd
Evan Rubinton <evanrubinton@gmail.com>
2015-03-22 21:22:18 -0400

    env check in init

--- config/initializers/load_vars.rb
+++ config/initializers/load_vars.rb
@@ -1,6 +1,5 @@
-ENV.update YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
-
 if Rails.env.development? || Rails.env.production?
+  ENV.update YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]
   ENV['VIDEO_PLAYER_DURATION'] = MediaModule.find_by(name: "Video Player").movie_duration.to_s
 end
 
