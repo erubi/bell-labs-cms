f716e7551274e8a9b49b1feecd5b91ec18b74737
Evan Rubinton <evanrubinton@gmail.com>
2015-04-01 12:04:35 -0400

    add rmagick gem, update image store directories

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.media_module.name}/#{mounted_as}"
+    "uploads/#{model.media_module.name}/#{mounted_as}/full"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -32,9 +32,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    def store_dir
+      "uploads/#{model.media_module.name}/#{mounted_as}/thumb"
+    end
+
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
