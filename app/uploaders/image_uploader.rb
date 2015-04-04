99a3c300c1c445bc7043db4b4579de3a03987573
Evan Rubinton <evanrubinton@gmail.com>
2015-04-04 13:16:12 -0400

    whitelist file tiff

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -89,7 +89,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png tif)
+    %w(jpg jpeg gif png tif tiff)
   end
 
   # Override the filename of the uploaded files:
