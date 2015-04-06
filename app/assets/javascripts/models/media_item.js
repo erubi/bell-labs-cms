a9dd84fa13ff22a0cbc8b3d22bae872394810ce9
Evan Rubinton <evanrubinton@gmail.com>
2015-04-06 17:23:23 -0400

    search metadata

--- app/assets/javascripts/models/media_item.js
+++ app/assets/javascripts/models/media_item.js
@@ -18,9 +18,38 @@ BellCMS.Models.MediaItem = Backbone.Model.extend({
   },
 
   hasInFilename: function(str){
-    if (this.get('filename').indexOf(str) != -1){
-    }
+    var lower = str.toLowerCase();
 
-    return this.get('filename').indexOf(str) != -1;
+    if (this.get('filename').indexOf(str) != -1){
+      return true;
+    } else if (this.get('filename').indexOf(lower) != -1){
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  hasInMetadata: function(str){
+    var bellLabsPeople = this.get('bell_labs_people') || '';
+    var topLevelCategory = this.get('top_level_category') || '';
+    var keywords = this.get('keywords') || '';
+    var additional = this.get('additional_metadata') || '';
+    var description= this.get('description') || '';
+
+    var lower = str.toLowerCase();
+
+    if (bellLabsPeople.indexOf(str) != -1 ||
+        topLevelCategory.indexOf(str) != -1 ||
+        additional.indexOf(str) != -1 ||
+        description.indexOf(str) != -1) {
+          return true;
+    } else if (bellLabsPeople.indexOf(lower) != -1 ||
+        topLevelCategory.indexOf(lower) != -1 ||
+        additional.indexOf(lower) != -1 ||
+        description.indexOf(lower) != -1) {
+          return true;
+    } else {
+      return false;
+    }
   }
 });
